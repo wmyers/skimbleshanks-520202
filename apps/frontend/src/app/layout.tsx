fd55c159c44212975fb5b36e7274d0be7a6a8bed
Will Myers <will@therecruitmentnetwork.com>
2025-02-23 20:02:18 +0000

    fix weird NextJS export font type bug

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-export const inter = Inter({ subsets: ['latin'] });
+// NB https://github.com/vercel/next.js/discussions/58693#discussioncomment-8024154
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'KK Tech Test',
